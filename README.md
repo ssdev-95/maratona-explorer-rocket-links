fbbe7817d438c32866f2fc3b7ddbb85f8a8d2636
Salomao S <souza95salomao@gmail.com>
2022-07-04 20:51:08 +0000

    update: code formatting and linting

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # maratona-explorer-rocket-linke
+
 A LinkTree like app made on Rocket seat's event Maratona Explorer.
--- src/components/active-link.tsx
+++ src/components/active-link.tsx
@@ -1,28 +1,18 @@
 import { ReactNode } from "react"
 
 type ActiveLinkProps = {
-	href: string;
-	title: string;
-	children: ReactNode;
+  href: string
+  title: string
+  children: ReactNode
 }
 
-export function ActiveLink({
-	children, href, title
-}:ActiveLinkProps) {
-	return (
-		<a
-			className="text-white group"
-			href={href}
-			target="_blank"
-		>
-			<div
-				className="bg-green-glass group-hover:bg-green-33 group-hover:text-darkblue-900 w-full p-4 rounded-md flex items-center justify-center gap-4 backdrop-opacity-10 backdrop-blur-sm"
-			>
-				{children}
-				<span className="text-xl">
-					{title}
-				</span>
-			</div>
-		</a>
-	)
+export function ActiveLink({ children, href, title }: ActiveLinkProps) {
+  return (
+    <a className="text-white group" href={href} target="_blank">
+      <div className="bg-green-glass group-hover:bg-green-33 group-hover:text-darkblue-900 w-full p-4 rounded-md flex items-center justify-center gap-4 backdrop-opacity-10 backdrop-blur-sm">
+        {children}
+        <span className="text-xl">{title}</span>
+      </div>
+    </a>
+  )
 }
